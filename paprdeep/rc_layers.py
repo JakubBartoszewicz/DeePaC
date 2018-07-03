002e86fd802dfc31cfec914c407262ac69d33672
AnjaSei <anjaseidel1@gmx.net>
2018-07-03 13:50:12 +0200

    added DenseAfterRevcompConv1D layer

--- paprdeep/rc_layers.py
+++ paprdeep/rc_layers.py
@@ -350,3 +350,54 @@ class DenseAfterRevcompWeightedSum(Dense):
         if self.activation is not None:
             output = self.activation(output)
         return output
+
+
+class DenseAfterRevcompConv1D(Dense):
+    '''For dense layers that follow 1D Convolutional or Pooling layers that
+    have reverse-complement weight sharing
+    '''
+
+    def build(self, input_shape):
+        assert len(input_shape) == 3, "layer designed to follow 1D conv/pool"
+        num_chan = input_shape[-1]
+        input_length = input_shape[-2]
+        assert num_chan%2 == 0,\
+         ("num_chan should be even if input is WeightedSum layer with"+
+          " input_is_revcomp_conv=True")
+        self.num_chan = num_chan
+        self.input_length = input_length
+
+        self.kernel = self.add_weight(shape=(int(input_length*num_chan/2), self.units),
+                                      initializer=self.kernel_initializer,
+                                      name='kernel',
+                                      regularizer=self.kernel_regularizer,
+                                      constraint=self.kernel_constraint)
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(self.units,),
+                                        initializer=self.bias_initializer,
+                                        name='bias',
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+        else:
+            self.bias = None
+        self.input_spec = InputSpec(min_ndim=2, axes={-1: self.num_chan})
+        self.built = True
+
+    def compute_output_shape(self, input_shape):
+        assert input_shape and len(input_shape) == 3
+        output_shape = [input_shape[0], self.units]
+        return tuple(output_shape)
+
+    def call(self, inputs):
+        kernel = K.reshape(self.kernel, (self.input_length, int(self.num_chan/2),
+                               self.units))
+        concatenated_reshaped_kernel = K.reshape(K.concatenate(
+            tensors=[kernel, kernel[::-1,::-1,:]], axis=1),
+            (self.input_length*self.num_chan, self.units))
+        reshaped_inputs = K.reshape(inputs, (-1, self.input_length*self.num_chan))
+        output = K.dot(reshaped_inputs, concatenated_reshaped_kernel)
+        if self.use_bias:
+            output = K.bias_add(output, self.bias, data_format='channels_last')
+        if self.activation is not None:
+            output = self.activation(output)
+        return output
