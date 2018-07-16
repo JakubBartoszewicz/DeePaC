445ae33eb605fecbb68f622e01fb731b02f99139
AnjaSei <anjaseidel1@gmx.net>
2018-07-16 11:23:42 +0200

    rename model + test data

--- deeplift/deeplift_conversion_sanity_check.py
+++ deeplift/deeplift_conversion_sanity_check.py
@@ -1,6 +1,5 @@
 
-
-#Install deeplift from https://github.com/kundajelab/deeplift/blob/keras2compat
+#Install deeplift from https://github.com/kundajelab/deeplift/
 import deeplift
 from deeplift.util import compile_func
 
@@ -11,10 +10,8 @@ from rc_layers import RevCompConv1D, RevCompConv1DBatchNorm, DenseAfterRevcompWe
 
 ###Creates deeplift model from keras model and checks whether the conversion was successful by comparing the predictions of the original model and the deeplift model
 
-#model_file = "nn-f128_l15_d64avg_pool-e001.h5"
-model_file = "nn-RC_f128_l15_d64avg_pool-e001.h5"
-#model_file = "cnn-10e7-new-fold1-e09.h5"
-test_data_file = "/scratch/seidela/data/test_data.npy"
+model_file = "cnn-10e7-new-fold1-e09.h5"
+test_data_file = "test_data.npy"
 
 #creates keras model and loads weights
 print("Loading keras model ...")
@@ -22,13 +19,11 @@ model = load_model(model_file, custom_objects={'RevCompConv1D': RevCompConv1D, '
 #convert keras model to deeplift model
 print("Loading deeplift model ...")
 deeplift_model = conversion.convert_model_from_saved_files(model_file, nonlinear_mxts_mode=deeplift.layers.NonlinearMxtsMode.DeepLIFT_GenomicsDefault) 
-print(deeplift_model.get_layers())
-#print(model.get_layer(index=0).get_weights()[0].shape)
-#print(model.get_layer(index=0).get_weights()[1].shape)
+
 
 print("Loading test data ...")
 x_test = np.load(test_data_file)
-x_test = x_test[0:1000,:,:]
+
 #keras model predictions
 print("Making predictions with the keras model ...")
 y_pred =  np.ndarray.flatten(model.predict_proba(x_test))
