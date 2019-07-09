ab2c418dbef426057df117a90e119af3ff0f3ef6
JakubBartoszewicz <kuba.bartoszewicz@gmail.com>
2019-07-09 16:32:30 +0200

    indents, cleaning

--- deepac/explain/gwpa/genomes_filter_activations.py
+++ deepac/explain/gwpa/genomes_filter_activations.py
@@ -7,8 +7,6 @@ import numpy as np
 from keras.models import load_model
 from keras import backend as K
 from Bio import SeqIO
-import tensorflow as tf
-from rc_layers import *
 
 '''
 Compute activation values genome-wide.
@@ -23,7 +21,7 @@ parser.add_argument("-o", "--out_dir", default = ".", help="Output directory")
 args = parser.parse_args()
 
 # Creates the model and loads weights
-model = load_model(args.model, custom_objects={'RevCompConv1D': RevCompConv1D, 'RevCompConv1DBatchNorm': RevCompConv1DBatchNorm, 'DenseAfterRevcompWeightedSum': DenseAfterRevcompWeightedSum, 'DenseAfterRevcompConv1D': DenseAfterRevcompConv1D})
+model = load_model(args.model)
 conv_layer_idx = [idx for idx, layer in enumerate(model.layers) if "Conv1D" in str(layer)][0]
 output_layer='conv1d_1'
 motif_length = model.get_layer(index=conv_layer_idx).get_weights()[0].shape[0]
@@ -31,9 +29,6 @@ pad_left = (motif_length - 1) // 2
 pad_right = motif_length - 1 - pad_left
 
 layer_dict = dict([(layer.name, layer) for layer in model.layers])
-RC_architecture = 'rev_comp_conv1d_1' in layer_dict.keys()
-if RC_architecture:
-	output_layer="rev_comp_conv1d_1"
 
 print("Loading test data (.npy) ...")
 test_data_set_name = os.path.splitext(os.path.basename(args.test_data))[0]
@@ -50,12 +45,12 @@ reads = ["N" * pad_left + r + "N" * pad_right for r in reads]
 #extract genome_id, genomic start and end positions of the reads
 reads_info = []
 for r in reads:
-	r_info = re.split(">|:|\.\.", r.id)
-	reads_info.append([r_info[0], int(r_info[1]), int(r_info[2])])
+    r_info = re.split(">|:|\.\.", r.id)
+    reads_info.append([r_info[0], int(r_info[1]), int(r_info[2])])
 
 #create output directory
 if not os.path.exists(args.out_dir):
-	os.makedirs(args.out_dir)
+    os.makedirs(args.out_dir)
 # Specify input and output of the network
 input_img = model.layers[0].input
 layer_output = layer_dict[output_layer].output
@@ -65,26 +60,26 @@ print("Computing activations ...")
 chunk_size = 10000
 n = 0
 while n < total_num_reads:
-	print("Done "+str(n)+" from "+str(total_num_reads)+" sequences")
-	samples_chunk = samples[n:n+chunk_size,:,:]
-	reads_info_chunk = reads_info[n:n+chunk_size]
+    print("Done "+str(n)+" from "+str(total_num_reads)+" sequences")
+    samples_chunk = samples[n:n+chunk_size,:,:]
+    reads_info_chunk = reads_info[n:n+chunk_size]
 
-	activations = iterate([samples_chunk, 0])[0] #activations.shape = [total_num_reads, len_reads, n_filters]
-	n_filters = activations.shape[-1]
-	for filter_index in range(n_filters):
-	
-		print("Processing filter " + str(filter_index) + " ...")
-		filter_bed_file = args.out_dir + "/" + test_data_set_name + "_filter_" + str(filter_index) + ".bed"
-	
-		pos_indices = np.where(activations[:,:,filter_index] > 0)
-		with open(filter_bed_file, 'a') as csv_file:
-			file_writer = csv.writer(csv_file, delimiter='\t')
-			for i  in range(len(pos_indices[0])):
-				read = pos_indices[0][i]
-				neuron = pos_indices[1][i]
-				genomic_start = neuron - pad_left + reads_info_chunk[read][1]
-				genomic_end = genomic_start + motif_length
-				activation_score = activations[read, neuron, filter_index]
-				file_writer.writerow([reads_info_chunk[read][0], max(0, genomic_start), genomic_end, "filter_"+str(filter_index), '%.4g' % activation_score])
+    activations = iterate([samples_chunk, 0])[0] #activations.shape = [total_num_reads, len_reads, n_filters]
+    n_filters = activations.shape[-1]
+    for filter_index in range(n_filters):
 
-	n += chunk_size
+        print("Processing filter " + str(filter_index) + " ...")
+        filter_bed_file = args.out_dir + "/" + test_data_set_name + "_filter_" + str(filter_index) + ".bed"
+
+        pos_indices = np.where(activations[:,:,filter_index] > 0)
+        with open(filter_bed_file, 'a') as csv_file:
+            file_writer = csv.writer(csv_file, delimiter='\t')
+            for i  in range(len(pos_indices[0])):
+                read = pos_indices[0][i]
+                neuron = pos_indices[1][i]
+                genomic_start = neuron - pad_left + reads_info_chunk[read][1]
+                genomic_end = genomic_start + motif_length
+                activation_score = activations[read, neuron, filter_index]
+                file_writer.writerow([reads_info_chunk[read][0], max(0, genomic_start), genomic_end, "filter_"+str(filter_index), '%.4g' % activation_score])
+
+    n += chunk_size
--- deepac/explain/gwpa/pathogenicity_gene_ranking.py
+++ deepac/explain/gwpa/pathogenicity_gene_ranking.py
@@ -23,15 +23,15 @@ if not os.path.exists(args.out_dir):
 
 def featuretype_filter(feature, featuretype):
 
-	if feature[2] == 'gene':
-		if feature.attrs.get('gene', None) == featuretype:
-			return True
-	
-	elif feature[2] in ["rRNA", "tRNA", "tmRNA", "ncRNA"]:
-		if feature.attrs.get('product', None) == featuretype:
-			return True
-	
-	return False
+    if feature[2] == 'gene':
+        if feature.attrs.get('gene', None) == featuretype:
+            return True
+
+    elif feature[2] in ["rRNA", "tRNA", "tmRNA", "ncRNA"]:
+        if feature.attrs.get('product', None) == featuretype:
+            return True
+
+    return False
 
 
 def subset_featuretypes(featuretype):
@@ -41,49 +41,49 @@ def subset_featuretypes(featuretype):
 
 def compute_gene_pathogenicity(filtered_gff):
 
-	#intersection = pybedtools.BedTool(bedgraph).intersect( b=filtered_gff)
-	intersection = bedgraph.intersect( b=filtered_gff)
-	total_num_bases = 0.
-	patho_score = 0.
-	for entry in intersection:
-		num_bases = entry.length
-		patho_score += float(entry.fields[3]) * num_bases
-		total_num_bases += num_bases
-	patho_score /= float(total_num_bases)
-	return patho_score
+    #intersection = pybedtools.BedTool(bedgraph).intersect( b=filtered_gff)
+    intersection = bedgraph.intersect( b=filtered_gff)
+    total_num_bases = 0.
+    patho_score = 0.
+    for entry in intersection:
+        num_bases = entry.length
+        patho_score += float(entry.fields[3]) * num_bases
+        total_num_bases += num_bases
+    patho_score /= float(total_num_bases)
+    return patho_score
 
 
 #for each species do
 for gff_file in os.listdir(args.gff_dir):
-	if gff_file.endswith(".gff"):
-		print("Processing " + gff_file + " ...")
-		gff = pybedtools.BedTool(args.gff_dir + "/" + gff_file)
-		bioproject_id = os.path.splitext(os.path.basename(gff_file))[0]
-		
-		#extract all feature types (genes, RNAs) from gff file
-		all_feature_types = []
-		for feature in gff:
-			if feature[2] == 'gene':
-				if 'gene' in feature.attrs:
-					all_feature_types.append(feature.attrs['gene'])
-			elif feature[2] in ["rRNA", "tRNA", "tmRNA", "ncRNA"]:
-				if 'product' in feature.attrs:
-					all_feature_types.append(feature.attrs['product'])
+    if gff_file.endswith(".gff"):
+        print("Processing " + gff_file + " ...")
+        gff = pybedtools.BedTool(args.gff_dir + "/" + gff_file)
+        bioproject_id = os.path.splitext(os.path.basename(gff_file))[0]
 
-		all_feature_types = sorted(list(set(all_feature_types)))
+        #extract all feature types (genes, RNAs) from gff file
+        all_feature_types = []
+        for feature in gff:
+            if feature[2] == 'gene':
+                if 'gene' in feature.attrs:
+                    all_feature_types.append(feature.attrs['gene'])
+            elif feature[2] in ["rRNA", "tRNA", "tmRNA", "ncRNA"]:
+                if 'product' in feature.attrs:
+                    all_feature_types.append(feature.attrs['product'])
 
-		patho_file = args.patho_dir + bioproject_id + "_fragmented_genomes_pathogenicity.bedgraph"
-		print("Processing " + patho_file + " ...")
-		bedgraph = pybedtools.BedTool(patho_file)
+        all_feature_types = sorted(list(set(all_feature_types)))
 
-		pool = multiprocessing.Pool(processes=20)
-		#filter gff files for feature of interest
-		filtered_gffs = pool.map(subset_featuretypes, all_feature_types)
-		#compute mean pathogencity score per feature
-		feature_pathogenicities = [compute_gene_pathogenicity(filtered_gff) for filtered_gff in filtered_gffs]
+        patho_file = args.patho_dir + bioproject_id + "_fragmented_genomes_pathogenicity.bedgraph"
+        print("Processing " + patho_file + " ...")
+        bedgraph = pybedtools.BedTool(patho_file)
 
-		#save results
-		patho_table = pd.DataFrame(OrderedDict( (('feature', all_feature_types), ('bioproject_id', bioproject_id), ('pathogenicity_score', feature_pathogenicities)) ))
-		patho_table = patho_table.sort_values(by=['pathogenicity_score'], ascending = False)
-		patho_table.to_csv(args.out_dir +  "/" + bioproject_id + "_feature_pathogenicity.csv" , sep = "\t", index = False)
-		pool.close()
+        pool = multiprocessing.Pool(processes=20)
+        #filter gff files for feature of interest
+        filtered_gffs = pool.map(subset_featuretypes, all_feature_types)
+        #compute mean pathogencity score per feature
+        feature_pathogenicities = [compute_gene_pathogenicity(filtered_gff) for filtered_gff in filtered_gffs]
+
+        #save results
+        patho_table = pd.DataFrame(OrderedDict( (('feature', all_feature_types), ('bioproject_id', bioproject_id), ('pathogenicity_score', feature_pathogenicities)) ))
+        patho_table = patho_table.sort_values(by=['pathogenicity_score'], ascending = False)
+        patho_table.to_csv(args.out_dir +  "/" + bioproject_id + "_feature_pathogenicity.csv" , sep = "\t", index = False)
+        pool.close()
