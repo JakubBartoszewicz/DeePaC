024fe8bb87c0d76d9d66f712f14d039c6c8e298e
JakubBartoszewicz <kuba.bartoszewicz@gmail.com>
2020-01-15 00:22:36 +0100

    handle weblogolib errors

--- deepac/explain/weblogos.py
+++ deepac/explain/weblogos.py
@@ -29,38 +29,43 @@ GC_content = np.sum(np.mean(np.mean(samples, axis = 1), axis = 0)[1:3])
 
 #for each convolutional filter
 for file in os.listdir(args.in_dir):
-	
-	if bool(re.search("_motifs_filter_[0-9]+.*" + args.file_ext, file)) and os.stat(args.in_dir + "/" + file).st_size > 0:
 
-		filter = re.search("filter_[0-9]+", file).group()
-		filter_index = filter.replace("filter_", "")
-		print("Processing filter: " + filter_index)
+    if bool(re.search("_motifs_filter_[0-9]+.*" + args.file_ext, file)) and os.stat(args.in_dir + "/" + file).st_size > 0:
 
-		fin = open(args.in_dir + "/" + file)
+        filter = re.search("filter_[0-9]+", file).group()
+        filter_index = filter.replace("filter_", "")
+        print("Processing filter: " + filter_index)
 
-		#load motifs from fasta file
-		if args.file_ext == ".fasta":
-			seqs = read_seq_data(fin) 
-			prior = weblogolib.parse_prior(str(GC_content), seqs.alphabet)
-			data = LogoData.from_seqs(seqs, prior)
-		
-		#load count matrix from transfac file
-		elif args.file_ext == ".transfac":
-		
-			from corebio.matrix import Motif
-			import weblogolib
-			motif = Motif.read_transfac(fin)
-			prior = weblogolib.parse_prior(str(GC_content), motif.alphabet)
-			data = weblogolib.LogoData.from_counts(motif.alphabet, motif, prior)
+        fin = open(args.in_dir + "/" + file)
 
-		#set logo options
-		options = LogoOptions()
-		options.logo_title = "filter " + filter_index
-		options.color_scheme = classic
-		options.stack_width = std_sizes["large"]
+        #load motifs from fasta file
+        if args.file_ext == ".fasta":
+            seqs = read_seq_data(fin)
+            prior = weblogolib.parse_prior(str(GC_content), seqs.alphabet)
+            data = LogoData.from_seqs(seqs, prior)
 
-		#save filter logo
-		format = LogoFormat(data, options)
-		jpeg = jpeg_formatter(data, format)
-		with open(args.out_dir + "/weblogo_" + file.replace(args.file_ext, ".jpeg"), 'wb') as out_file:
-			out_file.write(jpeg)
+        #load count matrix from transfac file
+        elif args.file_ext == ".transfac":
+
+            from corebio.matrix import Motif
+            import weblogolib
+            motif = Motif.read_transfac(fin)
+            prior = weblogolib.parse_prior(str(GC_content), motif.alphabet)
+            try:
+                data = weblogolib.LogoData.from_counts(motif.alphabet, motif, prior)
+            except ValueError as err:
+                print(err)
+                continue
+
+
+        #set logo options
+        options = LogoOptions()
+        options.logo_title = "filter " + filter_index
+        options.color_scheme = classic
+        options.stack_width = std_sizes["large"]
+
+        #save filter logo
+        format = LogoFormat(data, options)
+        jpeg = jpeg_formatter(data, format)
+        with open(args.out_dir + "/weblogo_" + file.replace(args.file_ext, ".jpeg"), 'wb') as out_file:
+            out_file.write(jpeg)
