43fde8063eec08f07d3158e6da9017789637be14
JakubBartoszewicz <kuba.bartoszewicz@gmail.com>
2020-09-12 15:41:55 +0200

    rf size in rc check

--- deepac/tests/rctest.py
+++ deepac/tests/rctest.py
@@ -2,7 +2,9 @@ import numpy as np
 import os
 import scipy
 import seaborn as sns
-from deepac.predict import predict_fasta, predict_npy
+from deepac.predict import predict_npy
+from deepac.explain.rf_sizes import get_rf_size
+import matplotlib.pyplot as plt
 
 
 def pred_fwd_rc(model, input_npy, output_fwd, output_rc):
@@ -11,14 +13,26 @@ def pred_fwd_rc(model, input_npy, output_fwd, output_rc):
     predict_npy(model, input_npy, output_rc, rc=True)
 
 
-def compare_rc(model, input_npy):
+def compare_rc(model, input_npy, kind="scatter", alpha=0.5):
     sns.set_context("paper", font_scale=2.5)
     out = "{}_predictions.png".format(os.path.splitext(input_npy)[0])
-    kind = "scatter"
-    marg_opt = dict(bins=100)
-    #joint_opt = dict(alpha=1.0)
+    if kind != "kde":
+        marg_opt = dict(bins=100, binrange=(0.0, 1.0))
+    else:
+        marg_opt = {}
+
+    if kind != "reg":
+        joint_opt = dict(alpha=alpha)
+    else:
+        joint_opt = {}
     pred_fwd = "{}_predictions.npy".format(os.path.splitext(input_npy)[0])
     pred_rc = "{}_rc_predictions.npy".format(os.path.splitext(input_npy)[0])
+
+    conv_layer_ids = [idx for idx, layer in enumerate(model.layers) if "Conv1D" in str(layer)]
+    if len(conv_layer_ids) > 0:
+        conv_layer_idx = conv_layer_ids[-1]
+        motif_length = get_rf_size(model, conv_layer_idx)
+        print("Receptive field size: {}".format(motif_length))
 
     pred_fwd_rc(model, input_npy, output_fwd=pred_fwd, output_rc=pred_rc)
 
@@ -28,10 +42,11 @@ def compare_rc(model, input_npy):
     print(scipy.stats.ks_2samp(fwd, rc))
     print(scipy.stats.spearmanr(fwd, rc))
 
-    g = (sns.jointplot(x=fwd, y=rc, kind=kind, color='k', marginal_kws=marg_opt)) \
+    g = (sns.jointplot(x=fwd, y=rc, kind=kind, color='k', joint_kws=joint_opt, marginal_kws=marg_opt)) \
         .set_axis_labels("forward", "reverse-complement")
 
     g.ax_joint.set_xticks([0, 0.5, 1])
     g.ax_joint.set_yticks([0, 0.5, 1])
 
     g.savefig(out)
+    plt.clf()
