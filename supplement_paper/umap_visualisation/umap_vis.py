47e4f688579d30a16bb4b8c5d0b64535344472be
MelaniaNowicka <melania.nowicka@gmail.com>
2021-02-23 18:48:47 +0100

    add: embedding save and load, unit tests

--- supplement_paper/umap_visualisation/umap_vis.py
+++ supplement_paper/umap_visualisation/umap_vis.py
@@ -5,10 +5,10 @@ import tensorflow as tf
 import random as rn
 import umap
 import utils
+import pickle
 import argparse
 import sys
 import os
-from datetime import datetime
 
 seed = 1
 
@@ -17,26 +17,35 @@ tf.random.set_seed(seed)
 rn.seed(seed)
 
 
+# check number of classes in the data set
 def check_number_of_classes(labels):
+
     classes = set(labels)
     print("Classes: ", classes)
 
     return classes
 
 
+# reshape the data - flatten reads from 3 to 2 dimensions
 def reshape_data(data):
+
     print("Reshaping data...")
     data_reshaped = data.reshape((data.shape[0], data.shape[1] * data.shape[2]))
 
     return data_reshaped
 
 
-def run_umap(data, n_components):
+# runs umap with default parameters
+def run_umap(path, data, n_components):
+
     print("Running umap...")
     reducer = umap.UMAP(random_state=42, n_components=n_components)
     reducer.fit(data)
 
+    print("Transforming data...")
     embedding = reducer.transform(data)
+
+    pickle.dump(reducer, open(os.path.join(path, "embedding.pickle"), 'wb'))
 
     assert (np.all(embedding == reducer.embedding_))
 
@@ -47,6 +56,7 @@ def run_umap(data, n_components):
 
 
 def create_scatter_plot(file_name, path, labels, classes, n_components, embedding):
+
     print("Creating plots...")
 
     fig = plt.figure()
@@ -63,7 +73,9 @@ def create_scatter_plot(file_name, path, labels, classes, n_components, embeddin
         scatter = ax.scatter(embedding[:, 0], embedding[:, 1], embedding[:, 2], c=labels, s=1, alpha=0.3)
         ax.legend(*scatter.legend_elements(), loc="upper right", title="Classes")
 
-    plot_name = "_".join(["scatter_plot", file_name.replace(".npy", "".join(["_", str(n_components)+"d", ".png"]))])
+    pre, ext = os.path.splitext(file_name)
+    plot_name = pre + ".png"
+    plot_name = "_".join(["scatter_plot", plot_name])
     # plt.gca().set_aspect('equal', 'datalim')
     # plt.colorbar(boundaries=np.arange(3)-0.5).set_ticks(np.arange(2))
     mpl.style.use("seaborn")
@@ -94,17 +106,25 @@ def run(args):
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-d', '--dataset-filename', dest="dataset_filename")
+    parser.add_argument('-d', '--dataset-filename', dest="dataset_filename", default=None)
+    parser.add_argument('-e', '--embedding-filename', dest='embedding_filename', default=None)
     parser.add_argument('-l', '--label-filename', dest="label_filename")
     parser.add_argument('-n', '--n-components', dest="n_components", type=int, default=2)
 
     params = parser.parse_args(args)
-
     file_name, path = utils.get_data_name(params.dataset_filename)
-    data, labels = utils.read_data_and_labels(params.dataset_filename, params.label_filename)
+
+    if params.embedding_filename is None:
+        data, labels = utils.read_data_and_labels(params.dataset_filename, params.label_filename)
+        data_reshaped = reshape_data(data)
+        embedding = run_umap(path, data_reshaped, params.n_components)
+    else:
+        data, labels = utils.read_data_and_labels(params.dataset_filename, params.label_filename)
+        umap_object = utils.read_embedding(params.embedding_filename)
+        data_reshaped = reshape_data(data)
+        embedding = umap_object.transform(data_reshaped)
+
     classes = check_number_of_classes(labels)
-    data_reshaped = reshape_data(data)
-    embedding = run_umap(data_reshaped, params.n_components)
     create_scatter_plot(file_name, path, labels, classes, params.n_components, embedding)
 
 
--- supplement_paper/umap_visualisation/utils.py
+++ supplement_paper/umap_visualisation/utils.py
@@ -1,9 +1,11 @@
 import numpy as np
+import pickle
 import sys
 import os
 
 
 def get_data_name(data_path):
+
     path_splitted = os.path.split(data_path)
     path = path_splitted[0]
     file_name = path_splitted[1]
@@ -12,6 +14,7 @@ def get_data_name(data_path):
 
 
 def read_data_and_labels(data_path, label_path):
+
     print("Reading data...")
     try:
         data = np.load(data_path)  # read the data
@@ -25,3 +28,17 @@ def read_data_and_labels(data_path, label_path):
     return data, labels
 
 
+def read_embedding(embedding_path):
+
+    print("Reading embedding...")
+    try:
+        openFile = open(embedding_path, 'rb')
+        umap_object = pickle.load(openFile)  # read the data
+        openFile.close()
+        print("Embedding succesfully read.")
+
+    except IOError:
+        print("Error: ", embedding_path, " not found.")
+        sys.exit(0)
+
+    return umap_object
