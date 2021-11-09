986f0b5a30df75d46a4436bcc8852bbb68195277
JakubBartoszewicz <kuba.bartoszewicz@gmail.com>
2021-11-09 22:23:25 +0100

    customize umap styles

--- supplement_paper/umap_visualisation/umap_vis.py
+++ supplement_paper/umap_visualisation/umap_vis.py
@@ -44,10 +44,11 @@ def run_umap(path, file_name, data, n_components, min_dist, n_neighbors, metric,
     return embedding
 
 
-def create_scatter_plot(file_name, path, parameters, labels, classes, n_components, embedding):
+def create_scatter_plot(file_name, path, parameters, labels, classes, n_components, embedding,
+                        style="seaborn", no_legend=False, class_names=None, alpha=0.8, size=3):
 
     print("Creating plots...")
-    plt.style.use("seaborn")
+    plt.style.use(style)
 
     fig = plt.figure()
 
@@ -55,24 +56,29 @@ def create_scatter_plot(file_name, path, parameters, labels, classes, n_componen
     if n_components == 1:
         ax = fig.add_subplot(111)
         for cs in classes:
-            ax.scatter(embedding[np.isclose(labels, cs), 0], range(len(embedding[np.isclose(labels, cs), 0])), s=1,
-                       alpha=0.3, label=cs)
+            ax.scatter(embedding[np.isclose(labels, cs), 0], range(len(embedding[np.isclose(labels, cs), 0])), s=size,
+                       alpha=alpha, label=cs)
     if n_components == 2:
         ax = fig.add_subplot(111)
         for cs in classes:
             ax.scatter(embedding[np.isclose(labels, cs), 0], embedding[np.isclose(labels, cs), 1],
-                       s=1, alpha=0.3, label=cs)
+                       s=size, alpha=alpha, label=cs)
     if n_components == 3:
         ax = fig.add_subplot(111, projection='3d')
         for cs in classes:
             ax.scatter(embedding[np.isclose(labels, cs), 0], embedding[np.isclose(labels, cs), 1],
-                       embedding[np.isclose(labels, cs), 2], s=1, alpha=0.3, label=cs)
+                       embedding[np.isclose(labels, cs), 2], s=size, alpha=alpha, label=cs)
 
     plot_name = "_".join(["scatter_plot", file_name, parameters]) + ".png"
     # plt.gca().set_aspect('equal', 'datalim')
     # plt.colorbar(boundaries=np.arange(3)-0.5).set_ticks(np.arange(2))
-    plt.legend(markerscale=5.0)
-    plt.title(plot_name.replace(".png", ""), fontsize=12)
+    if not no_legend:
+        if class_names is not None:
+            labels = class_names.split(";")
+            plt.legend(labels=labels, markerscale=5.0)
+        else:
+            plt.legend(markerscale=5.0)
+    # plt.title(plot_name.replace(".png", ""), fontsize=12)
     plt.savefig(os.path.join(path, plot_name))
     plt.close()
 
@@ -81,27 +87,28 @@ def create_scatter_plot(file_name, path, parameters, labels, classes, n_componen
         fig = plt.figure()
         if n_components == 1:
             ax = fig.add_subplot(111)
-            ax.scatter(embedding[np.isclose(labels, cs), 0], range(len(embedding[np.isclose(labels, cs), 0])), s=1,
-                       alpha=0.3)
+            ax.scatter(embedding[np.isclose(labels, cs), 0], range(len(embedding[np.isclose(labels, cs), 0])), s=size,
+                       alpha=alpha)
         if n_components == 2:
             ax = fig.add_subplot(111)
             ax.scatter(embedding[np.isclose(labels, cs), 0], embedding[np.isclose(labels, cs), 1],
-                       s=1, alpha=0.3)
+                       s=size, alpha=alpha)
         if n_components == 3:
             ax = fig.add_subplot(111, projection='3d')
             ax.scatter(embedding[np.isclose(labels, cs), 0], embedding[np.isclose(labels, cs), 1],
-                       embedding[np.isclose(labels, cs), 2], s=1, alpha=0.3)
+                       embedding[np.isclose(labels, cs), 2], s=size, alpha=alpha)
 
         plot_name_temp = plot_name.replace("scatter_plot", "_".join(["scatter_plot", str(cs)]))
-        plt.title(plot_name_temp.replace(".png", ""), fontsize=12)
+        # plt.title(plot_name_temp.replace(".png", ""), fontsize=12)
         plt.savefig(os.path.join(path, plot_name_temp))
         plt.close()
 
 
 # generates plots that highlight given classes
-def highlight_classes(file_name, path, labels, hl_classes, n_components, embedding):
+def highlight_classes(file_name, path, labels, hl_classes, n_components, embedding,
+                      style="seaborn", no_legend=False, class_names=None, alpha=0.8, size=3):
 
-    plt.style.use("seaborn")
+    plt.style.use(style)
 
     print("Highlighting classes...")
     for ind in hl_classes:
@@ -115,31 +122,36 @@ def highlight_classes(file_name, path, labels, hl_classes, n_components, embeddi
         if n_components == 1:
             ax = fig.add_subplot(111)
             for cs in set(temp_labels):
-                ax.scatter(embedding[temp_labels == cs, 0], range(len(embedding[temp_labels == cs, 0])), s=1, alpha=0.3,
-                           label=cs)
+                ax.scatter(embedding[temp_labels == cs, 0], range(len(embedding[temp_labels == cs, 0])), s=size,
+                           alpha=alpha, label=cs)
         if n_components == 2:
             ax = fig.add_subplot(111)
             for cs in set(temp_labels):
                 ax.scatter(embedding[temp_labels == cs, 0], embedding[temp_labels == cs, 1],
-                           s=1, alpha=0.3, label=cs)
+                           s=size, alpha=alpha, label=cs)
         if n_components == 3:
             ax = fig.add_subplot(111, projection='3d')
             for cs in set(temp_labels):
                 ax.scatter(embedding[temp_labels == cs, 0], embedding[temp_labels == cs, 1],
-                           embedding[temp_labels == cs, 2], s=1, alpha=0.3, label=cs)
+                           embedding[temp_labels == cs, 2], s=size, alpha=alpha, label=cs)
 
         plot_name = "hl_class" + str(ind) + "_" + file_name + ".png"
         plot_name = "_".join(["scatter_plot", plot_name])
         # plt.gca().set_aspect('equal', 'datalim')
         # plt.colorbar(boundaries=np.arange(3)-0.5).set_ticks(np.arange(2))
-        plt.legend(markerscale=5.0)
-        plt.title(plot_name.replace(".png", ""), fontsize=12)
+        if not no_legend:
+            if class_names is not None:
+                labels = class_names.split(";")
+                plt.legend(labels=labels, markerscale=5.0)
+            else:
+                plt.legend(markerscale=5.0)
+        # plt.title(plot_name.replace(".png", ""), fontsize=12)
         plt.savefig(os.path.join(path, plot_name))
         plt.close()
 
 
 def run_workflow(dataset_filename, embedding_filename, label_filename, n_components, min_dist, n_neighbors, metric,
-                 seed, highlight_classes):
+                 seed, do_highlight_classes, style, no_legend, class_names, alpha, size):
 
     file_name, path = utils.get_data_name(dataset_filename)
 
@@ -172,13 +184,17 @@ def run_workflow(dataset_filename, embedding_filename, label_filename, n_compone
     # check the number of classes in labels
     classes = utils.check_number_of_classes(labels)
 
+    np.save(file=os.path.join(path, file_name + "_coords.npy"), arr=embedding)
+
     # plot embeddings
-    if highlight_classes is not None:  # plot highlight classes
-        hl_classes = highlight_classes.split(";")
-        highlight_classes(f_name, path, labels, hl_classes, n_components, embedding)
+    if do_highlight_classes is not None:  # plot highlight classes
+        hl_classes = do_highlight_classes.split(";")
+        highlight_classes(f_name, path, labels, hl_classes, n_components, embedding,
+                          style, no_legend, class_names, alpha, size)
     else:  # plot all and separate class plots
         parameters = "_".join([str(min_dist).replace(".", ""), str(n_neighbors), str(metric), str(n_components) + "d"])
-        create_scatter_plot(f_name, path, parameters, labels, classes, n_components, embedding)
+        create_scatter_plot(f_name, path, parameters, labels, classes, n_components, embedding,
+                            style, no_legend, class_names, alpha, size)
 
 
 def parse_and_run(args):
@@ -191,15 +207,21 @@ def parse_and_run(args):
     parser.add_argument('-l', '--label-filename', dest="label_filename")
     parser.add_argument('-n', '--n-components', dest="n_components", type=int, default=2)
     parser.add_argument('-m', '--min-dist', dest="min_dist", type=float, default=0.1)
-    parser.add_argument('-b', '--n_neighbors', dest="n_neighbors", type=int, default=15)
+    parser.add_argument('-b', '--n-neighbors', dest="n_neighbors", type=int, default=15)
     parser.add_argument('-t', '--metric', dest="metric", type=str, default='euclidean')
     parser.add_argument('-s', '--seed', dest="seed", type=int, default=None)
-    parser.add_argument('-c', '--highlight_classes', dest='highlight_classes', type=str, default=None)
+    parser.add_argument('-c', '--highlight-classes', dest='highlight_classes', type=str, default=None)
+    parser.add_argument('-C', '--class-names', dest='class_names', type=str, default=None)
+    parser.add_argument('--style', dest='style', type=str, default="seaborn")
+    parser.add_argument('--no-legend', dest='no_legend', type=bool, default=False, action="store_true")
+    parser.add_argument('--alpha', dest='alpha', type=float, default=0.8)
+    parser.add_argument('--size', dest='size', type=float, default=3)
 
     params = parser.parse_args(args)
 
     run_workflow(params.dataset_filename, params.embedding_filename, params.label_filename, params.n_components,
-                 params.min_dist, params.n_neighbors, params.metric, params.seed, params.highlight_classes)
+                 params.min_dist, params.n_neighbors, params.metric, params.seed, params.highlight_classes,
+                 params.style, params.no_legend, params.class_names, params.alpha, params.size)
 
 
 if __name__ == "__main__":
