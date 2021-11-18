70cc5f396cb94823585d38e2294456e71928fef8
JakubBartoszewicz <kuba.bartoszewicz@gmail.com>
2021-11-19 00:39:33 +0100

    fungal/multiclass model repo, version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='deepac',
-      version='0.13.6',
+      version='0.14.0',
       description='Predicting pathogenic potentials of novel DNA with reverse-complement neural networks.',
       long_description=readme(),
       long_description_content_type='text/markdown',
