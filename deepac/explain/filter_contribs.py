92903fe618327d27194e53933e6ad11b2388296b
JakubBartoszewicz <kuba.bartoszewicz@gmail.com>
2023-06-05 18:30:53 +0200

    fix indentation style

--- deepac/explain/filter_contribs.py
+++ deepac/explain/filter_contribs.py
@@ -218,11 +218,11 @@ def get_filter_contribs(args, allow_eager=False):
                     scores_fwd = scores_fwd[:, :, n_filters:]
 
                     dat_fwd = p.map(partial(get_filter_data, scores_filter_avg=scores_fwd,
-                                               input_reads=reads_chunk, motif_len=motif_length,
-                                               max_only=max_only), filter_range)
+                                            input_reads=reads_chunk, motif_len=motif_length,
+                                            max_only=max_only), filter_range)
                     dat_rc = p.map(partial(get_filter_data, scores_filter_avg=scores_rc,
-                                              input_reads=reads_chunk, motif_len=motif_length, rc=True,
-                                              max_only=max_only), filter_range)
+                                           input_reads=reads_chunk, motif_len=motif_length, rc=True,
+                                           max_only=max_only), filter_range)
 
             if max_only:
                 dat_max = p.map(partial(get_max_strand, dat_fwd=dat_fwd, dat_rc=dat_rc), filter_range)
@@ -234,11 +234,11 @@ def get_filter_contribs(args, allow_eager=False):
         print("Saving data ...")
         with get_context("spawn").Pool(processes=min(cores, n_filters)) as p:
             if contrib_dat_fwd:
-                    p.map(partial(write_filter_data, contribution_data=contrib_dat_fwd, motifs=motif_dat_fwd,
-                                      out_dir=args.out_dir, data_set_name=test_data_set_name), filter_range)
+                p.map(partial(write_filter_data, contribution_data=contrib_dat_fwd, motifs=motif_dat_fwd,
+                              out_dir=args.out_dir, data_set_name=test_data_set_name), filter_range)
             if contrib_dat_rc:
-                    p.map(partial(write_filter_data,  contribution_data=contrib_dat_rc, motifs=motif_dat_rc,
-                                      out_dir=args.out_dir, data_set_name=test_data_set_name), filter_range)
+                p.map(partial(write_filter_data,  contribution_data=contrib_dat_rc, motifs=motif_dat_rc,
+                              out_dir=args.out_dir, data_set_name=test_data_set_name), filter_range)
 
         if args.partial:
             print("Getting partial data ...")
